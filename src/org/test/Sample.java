ae50e732a65cc16b932b8c421edaed865f597d8d
selvapriyak31 <kselvapriya31@gmail.com>
2022-07-20 15:44:28 +0530

    project structure

---
+++ src/org/test/Sample.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class Sample {
+	public static void main(String[] args)
+	{
+		for(int row=0;row<=7;row++){
+			for(int col=7-row;col>1;col--) {
+			System.out.print("*");
+	}
+			for(int col=0;col<=row;col++)
+			{
+				System.out.print("*");
+
+			}
+			System.out.println("*");
+}}}
+
